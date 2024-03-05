bd7ee9887fefd3453ba91b6c73605adca7935daa
Sergey Bondarev <bondarevsergeq1@mail.ru>
2024-03-05 23:24:40 +0700

    Change code

--- src/test/java/SrvcCalculateTest.java
+++ src/test/java/SrvcCalculateTest.java
@@ -1,10 +1,11 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.services.SrvcCalculate;
+
 public class SrvcCalculateTest {
 
     @Test
-    public void setActualMounth(){
+    public void setActualMounth() {
         SrvcCalculate service = new SrvcCalculate();
         int expected = 2;
         int actual = service.calculate(100_000, 60_000, 150_000);
