6c94d46187d03bf78ee211e544e99c3aba3ba9d7
Sergey Bondarev <bondarevsergeq1@mail.ru>
2024-03-05 23:21:19 +0700

    initial commit

---
+++ src/test/java/SrvcCalculateTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.services.SrvcCalculate;
+public class SrvcCalculateTest {
+
+    @Test
+    public void setActualMounth(){
+        SrvcCalculate service = new SrvcCalculate();
+        int expected = 2;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        Assertions.assertEquals(expected, actual);
+    }
+}
