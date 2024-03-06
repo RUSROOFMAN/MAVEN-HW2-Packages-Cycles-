2474452fe496d3817d9e4811fc63ab5aedf51b60
Sergey Bondarev <bondarevsergeq1@mail.ru>
2024-03-06 15:33:42 +0700

    Change code 2

--- src/main/java/ru/netology/services/SrvcCalculate.java
+++ src/main/java/ru/netology/services/SrvcCalculate.java
@@ -6,10 +6,12 @@ public class SrvcCalculate {
         int money = 0; // количество денег на счету
         for (int month = 1; month <= 12; month++) {
             if (money < threshold) { // можем ли отдыхать?
+                System.out.println(" Месяц " + month + "." + " Денег " + money + "." + " Придётся работать. Заработал  +" + income + " потратил  -" + expenses);
                 money = money + income - expenses;
             } else {
                 int vacMoneyBalance = money - expenses;
                 int vacMoney = vacMoneyBalance - ((money - expenses) / 3);
+                System.out.println(" Месяц " + month + " Денег " + money + " Буду отдыхать." + "Потратил" + expenses + ",затем ещё  - " + money);
                 money = (money - expenses) / 3;
                 countVacation++;
             }
